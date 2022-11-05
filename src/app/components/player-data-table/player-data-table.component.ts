98f59d945cb14edb57a142c8163a138f740fbbc3
Steve Potts <steve.potts@tappit.com>
2022-11-05 20:40:23 +0000

    so far

--- src/app/components/player-data-table/player-data-table.component.ts
+++ src/app/components/player-data-table/player-data-table.component.ts
@@ -1,5 +1,6 @@
 import {  Component, OnInit, ViewChild } from '@angular/core';
 import { MatSort, Sort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { PlayerData } from 'src/app/interfaces/player-data.interface';
 import { Stats } from 'src/app/interfaces/stats.interface';
 import { PlayerDataService } from 'src/app/_services/player-data.service';
@@ -11,17 +12,21 @@ import { default as PLAYERDATA } from '../../_data/players.json';
   styleUrls: ['./player-data-table.component.scss']
 })
 export class PlayerDataTableComponent implements OnInit {
-  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
   displayedColumns: string[] = ['orderBy', 'Name', 'GamesPlayed', 'Score'];
-  dataSource;
+  dataSource: MatTableDataSource<any> = new MatTableDataSource();
   playerArray: any = PLAYERDATA.Players;
   statsArray;
-  playerData: PlayerData[] = [];
+  playerData = [];
+  tableData: any;
+  topThreePlayers = [];
+
   constructor(private playerDataService: PlayerDataService) { }
 
   ngOnInit(): void {
     this.playerDataService.getPlayerStatsData().subscribe((data: any) => {
       this.statsArray = data;
+      console.log(this.statsArray);
       for (let i = 0; i < this.playerArray.length; i++) {
         this.playerData.push({
           ...this.playerArray[i],
@@ -29,15 +34,17 @@ export class PlayerDataTableComponent implements OnInit {
         });
       }
 
-      this.dataSource = this.playerData;
-      this.dataSource.sort((a: any,b: any) => (b.Score - a.Score));
-      this.dataSource = this.dataSource.map((item: any, idx: any) => (
+      this.dataSource.data = this.playerData;
+      this.dataSource.data = this.dataSource.data.map((item: any, idx: any) => (
         {
           ...item,
           orderBy: idx + 1,
           player: `${item.Name} ${item.Score} ${idx}`
         }
       ));
+      this.dataSource.sort = this.sort;
+      this.topThreePlayers = this.dataSource.data.sort((a,b) => b.Score - a.Score).slice(0, 3)
+      console.log(this.topThreePlayers);
     });
   }
 
@@ -45,4 +52,9 @@ export class PlayerDataTableComponent implements OnInit {
     console.log(sort);
     this.dataSource.sort = this.sort; 
   }
+
+  filterData(event) {
+    this.dataSource.filter = event.target.value.trim().toLowerCase();
+  }
+
 }
